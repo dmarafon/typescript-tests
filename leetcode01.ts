1ec09b76ab870ebf258369706ddf384ee19088e7
Daniell Marafon <daniellmarafon@gmail.com>
2022-11-10 20:15:31 +0100

    feat(leetcode): add exercise 01 resolved

---
+++ leetcode01.ts
@@ -0,0 +1,61 @@
+/** 
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+Source: https://leetcode.com/problems/two-sum/description/
+
+/** */
+
+const twoSum = (nums: number[], target: number): number[] => {
+  const hashMap: Map<number, number> = new Map();
+
+  nums.forEach((num, index) => hashMap.set(index, target - num));
+
+  let result: number[] = [];
+
+  hashMap.forEach((value, key) => {
+    if (result.length === 0) {
+      for (let i = 0; i < nums.length; i++) {
+        if (i !== key && nums[i] === value) {
+          console.log(key, i);
+          result.push(key);
+          result.push(i);
+          return;
+        }
+      }
+    }
+  });
+  return result;
+};
+
+twoSum([2, 7, 11, 15], 9);
+
+twoSum([3, 2, 4], 6);
+
+twoSum([3, 3], 6);
